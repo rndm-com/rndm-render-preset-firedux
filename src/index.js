8f159ca57fa485eeec48f73c8b8aa0c77e43176c
Paul Napier <>
2018-12-19 15:13:07 +1100

    Preparation for release

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { Base } from '@rndm/render-plugin-firebase';
 import { use } from '@rndm/render';
+import * as firebase from '@rndm/render-plugin-firebase';
+import * as redux from '@rndm/render-plugin-redux';
+
 import components from './components';
 import Firebase from './components/Firebase';
 
@@ -12,6 +14,8 @@ use(plugin);
 
 export {
   Firebase,
-}
+  firebase,
+  redux,
+};
 
 export default plugin;
